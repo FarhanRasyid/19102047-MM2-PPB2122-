a4981a6aa7e23c90c73e398520adc83ef5d123ad
Farhan Rasyid Kamaludin <19102047@ittelkom-pwt.ac.id>
2022-01-20 21:02:44 +0700

    Practice9

---
+++ Practice9/laravel/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+Route::post('/student', 'StudentController@store')->name('store')->middleware('login_auth');;
+Route::get('/student/create', 'StudentController@create')->name('create')->middleware('login_auth');;
+Route::get('/student', 'StudentController@index')->name('index')->middleware('login_auth');;
+Route::get('/student/{student}', 'StudentController@show')->name('student.show')->middleware('login_auth');;
+Route::get('/student/{student}/edit', 'StudentController@edit')->name('student.edit')->middleware('login_auth');;
+Route::patch('/student/{student}', 'StudentController@update')->name('student.update')->middleware('login_auth');;
+Route::delete('/student/{student}', 'StudentController@destroy')->name('student.destroy')->middleware('login_auth');;
+
+//admin route
+Route::get('/login', 'AdminController@index')->name('login.index');
+Route::get('/logout', 'AdminController@logout')->name('login.logout');
+Route::post('/login', 'AdminController@process')->name('login.process');
+
+Route::get('/adminlte/index', 'AdminLTEController@index')->name('adminlte.index');
+Route::get('/adminlte/student/create', 'AdminLTEStudentController@create')->name('adminlte.student.create');
